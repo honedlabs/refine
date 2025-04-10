c4068ae457bdea36584679b2d9aa778d0eddd327
jdw5 <87954417+jdw5@users.noreply.github.com>
2025-04-10 22:40:36 +0000

    Fix styling

--- src/DataFilter.php
+++ src/DataFilter.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Honed\Refine;
 
-use Honed\Core\Contracts\HasQuery;
-use Honed\Refine\Contracts\DefinesOptions;
 use Illuminate\Support\Arr;
 
 /**
@@ -18,7 +16,7 @@ final class DataFilter extends Filter
 {
     /**
      * The clauses this filter supports.
-     * 
+     *
      * @var array<int,\Honed\Refine\Enums\Clause>
      */
     protected $clauses = [];
@@ -33,7 +31,7 @@ final class DataFilter extends Filter
 
     /**
      * Set the clauses this filter supports.
-     * 
+     *
      * @param  \Honed\Refine\Enums\Clause|array<int,\Honed\Refine\Enums\Clause>  ...$clauses
      * @return $this
      */
@@ -48,7 +46,7 @@ final class DataFilter extends Filter
 
     /**
      * Get the clauses this filter supports.
-     * 
+     *
      * @return array<int,\Honed\Refine\Enums\Clause>
      */
     public function getClauses()
